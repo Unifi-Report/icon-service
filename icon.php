ae09eb17c06bd61c9695d96fd58a82c0c038a575
Bart <bart@meierij-it.nl>
2020-10-02 13:27:08 +0200

    Update icon.php

--- icon.php
+++ icon.php
@@ -2,18 +2,16 @@
 $selectedToken = $_GET['token'];
 
 
-$json_TronscanIcons = file_get_contents('https://apilist.tronscan.org/api/token_trc20?contract=' . $selectedToken . '');
+$json_TronscanIcons = file_get_contents('https://apilist.tronscan.org/api/token?id=' . $selectedToken . '');
 $TronscanIcons = json_decode($json_TronscanIcons);
 
-$tronscanIconData = $TronscanIcons->trc20_tokens;
+$tronscanIconData = $TronscanIcons->data;
 
 
-//print $tronscanIconData[0]->icon_url;
 if ($tronscanIconData[0]->icon_url){
-  $homepage = file_get_contents( $tronscanIconData[0]->icon_url);
+  $homepage = file_get_contents( $tronscanIconData[0]->imgUrl);
 }else{
-  $homepage = 'https://raw.githubusercontent.com/BitGuildPlatform/dapps/master/tron/trc10/' . $selectedToken . '.png';
+  $homepage = file_get_contents('https://raw.githubusercontent.com/BitGuildPlatform/dapps/master/tron/trc10/' . $selectedToken . '.png');
 }
-
 
 echo $homepage;
