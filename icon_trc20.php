f4da209b7d72378cea866a37fde43845ff5e2474
Bart <bart@meierij-it.nl>
2020-10-07 16:14:45 +0200

    Update icon_trc20.php

--- icon_trc20.php
+++ icon_trc20.php
@@ -2,25 +2,24 @@
 $selectedToken = $_GET['token'];
 
 
-
-$filename = 'icons/tron/trc20/' . $selectedToken .'.png';
+$filename = 'icons/tron/trc20/' . $selectedToken . '.png';
 
 //print $tronscanIconData[0]->icon_url;
 
-if (file_exists($filename))  {
+if (file_exists($filename)) {
 
-    $image = file_get_contents('icons/tron/trc20/' . $selectedToken .'.png');
+    $image = file_get_contents('icons/tron/trc20/' . $selectedToken . '.png');
 
 
-}else{
+} else {
     $json_TronscanIcons = file_get_contents('https://apilist.tronscan.org/api/token_trc20?contract=' . $selectedToken . '');
     $TronscanIcons = json_decode($json_TronscanIcons);
 
     $tronscanIconData = $TronscanIcons->trc20_tokens;
-    if ($tronscanIconData[0]->icon_url == 'https://coin.top/production/upload/logo/default.png'){
-      $image = file_get_contents('https://raw.githubusercontent.com/BitGuildPlatform/dapps/master/tron/trc20/' . $selectedToken . '.png');
-    }else{
-      $image = file_get_contents( $tronscanIconData[0]->icon_url);
+    if ($tronscanIconData[0]->icon_url == 'https://coin.top/production/upload/logo/default.png') {
+        $image = file_get_contents('https://raw.githubusercontent.com/BitGuildPlatform/dapps/master/tron/trc20/' . $selectedToken . '.png');
+    } else {
+        $image = file_get_contents($tronscanIconData[0]->icon_url);
     }
 }
 
