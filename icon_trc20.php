ab6e88f9919cf7971000a76814c8265a69be2005
Bart <bart@meierij-it.nl>
2020-11-02 15:53:49 +0100

    Update icon_trc20.php

--- icon_trc20.php
+++ icon_trc20.php
@@ -7,14 +7,10 @@ $filename = 'icons/tron/trc20/' . $selectedToken . '.png';
 //print $tronscanIconData[0]->icon_url;
 
 if (file_exists($filename)) {
-
     $image = file_get_contents('icons/tron/trc20/' . $selectedToken . '.png');
-
-
 } else {
     $json_TronscanIcons = file_get_contents('https://apilist.tronscan.org/api/token_trc20?contract=' . $selectedToken . '');
     $TronscanIcons = json_decode($json_TronscanIcons);
-
     $tronscanIconData = $TronscanIcons->trc20_tokens;
     if ($tronscanIconData[0]->icon_url == 'https://coin.top/production/upload/logo/default.png') {
         $image = file_get_contents('https://raw.githubusercontent.com/BitGuildPlatform/dapps/master/tron/trc20/' . $selectedToken . '.png');
