8afc92e2fef09a506e0bcb4e25e42ae89fbaf4b9
Bart <bart@meierij-it.nl>
2021-01-06 16:08:34 +0100

    Mix an match v0.1

--- mixAndMatch.php
+++ mixAndMatch.php
@@ -1,1 +1,23 @@
 <?php
+// Get unifi Tokens pairs
+$json_unifiTokens = file_get_contents('data/tokens.json');
+$unifiTokens = json_decode($json_unifiTokens);
+
+// get smartContract from url
+$smartContract = $_GET['smartContract'];
+
+
+foreach ($unifiTokens as $unifiToken){
+  if ($smartContract == $unifiToken->smartContract){
+    $BlockchainShort = $unifiToken->BlockchainShort;
+    $tokenAddress = $unifiToken->tokenAddress;
+  }
+}
+
+
+
+$image = file_get_contents('https://icon-service.unifi.report/icon_trc20?token=' . $tokenAddress . '.png');
+
+
+
+echo $image;
