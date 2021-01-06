81c89c444f296921b6759eb6191d615db32ea63b
Bart <bart@meierij-it.nl>
2021-01-06 16:59:28 +0100

    adding name support

--- mixAndMatch.php
+++ mixAndMatch.php
@@ -6,30 +6,60 @@ $unifiTokens = json_decode($json_unifiTokens);
 // get smartContract from url
 $smartContract = $_GET['smartContract'];
 
-
-foreach ($unifiTokens as $unifiToken){
-  if ($smartContract == $unifiToken->smartContract){
-    $BlockchainShort = $unifiToken->BlockchainShort;
-    $tokenAddress = $unifiToken->tokenAddress;
+if ($smartContract):
+  foreach ($unifiTokens as $unifiToken){
+    if ($smartContract == $unifiToken->smartContract){
+      $BlockchainShort = $unifiToken->BlockchainShort;
+      $tokenAddress = $unifiToken->tokenAddress;
+    }
   }
-}
 
+  if ($BlockchainShort == 'TRX'){
+    $image = file_get_contents('https://icon-service.unifi.report/icon_trc20?token=' . $tokenAddress . '');
+  }
+  if ($BlockchainShort == 'ETH'){
+    $image = file_get_contents('https://icon-service.unifi.report/icon_eth?token=' . $tokenAddress . '');
+  }
+  if ($BlockchainShort =='ICX'){
+    $image = file_get_contents('https://icon-service.unifi.report/icon_icx?token=' . $tokenAddress . '');
+  }
+  if ($BlockchainShort == 'BSC'){
+    $image = file_get_contents('https://icon-service.unifi.report/icon_bsc?token=' . $tokenAddress . '');
+  }
+  if ($BlockchainShort == 'ONE'){
+    $image = file_get_contents('https://icon-service.unifi.report/icon_one?token=' . $tokenAddress . '');
+  }
+endif;
 
-if ($BlockchainShort == 'TRX'){
-  $image = file_get_contents('https://icon-service.unifi.report/icon_trc20?token=' . $tokenAddress . '');
-}
-if ($BlockchainShort == 'ETH'){
-  $image = file_get_contents('https://icon-service.unifi.report/icon_eth?token=' . $tokenAddress . '');
-}
-if ($BlockchainShort =='ICX'){
-  $image = file_get_contents('https://icon-service.unifi.report/icon_icx?token=' . $tokenAddress . '');
-}
-if ($BlockchainShort == 'BSC'){
-  $image = file_get_contents('https://icon-service.unifi.report/icon_bsc?token=' . $tokenAddress . '');
-}
-if ($BlockchainShort == 'ONE'){
-  $image = file_get_contents('https://icon-service.unifi.report/icon_one?token=' . $tokenAddress . '');
-}
+$blockchain = $_GET['blockchain'];
+$tokenName = $_GET['tokenName'];
+if ($blockchain):
+  foreach ($unifiTokens as $unifiToken){
+    if ($blockchain == $unifiToken->Blockchain){
+      if ($tokenName == $unifiToken->name){
+        $BlockchainShort = $unifiToken->BlockchainShort;
+        $tokenAddress = $unifiToken->tokenAddress;
+      }
+    }
+  }
+
+  if ($BlockchainShort == 'TRX'){
+    $image = file_get_contents('https://icon-service.unifi.report/icon_trc20?token=' . $tokenAddress . '');
+  }
+  if ($BlockchainShort == 'ETH'){
+    $image = file_get_contents('https://icon-service.unifi.report/icon_eth?token=' . $tokenAddress . '');
+  }
+  if ($BlockchainShort =='ICX'){
+    $image = file_get_contents('https://icon-service.unifi.report/icon_icx?token=' . $tokenAddress . '');
+  }
+  if ($BlockchainShort == 'BSC'){
+    $image = file_get_contents('https://icon-service.unifi.report/icon_bsc?token=' . $tokenAddress . '');
+  }
+  if ($BlockchainShort == 'ONE'){
+    $image = file_get_contents('https://icon-service.unifi.report/icon_one?token=' . $tokenAddress . '');
+  }
+
+endif;
 
 
 echo $image;
