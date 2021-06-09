29578e12646842bf68e76448cd013fe82b033e24
Bart <bart@meierij-it.nl>
2021-06-09 20:29:42 +0200

    Update icon_bsc.php

--- icon_bsc.php
+++ icon_bsc.php
@@ -5,15 +5,10 @@ $filename = 'icons/binanceSmartChain/' . $selectedToken . '.png';
 if (file_exists($filename)) {
   $image = file_get_contents('icons/binanceSmartChain/' . $selectedToken . '.png');
 } else {
-  // https://token-data.unifi.report/api/getInfo?token=0x2a93172c8DCCbfBC60a39d56183B7279a2F647b4&chain=BSC
-//  $jsonTokenData = file_get_contents('https://token-data.unifi.report/api/getInfo?token=' . $selectedToken . '&chain=BSC');
-//  $tokenData = json_decode($jsonTokenData);
-//  if ($tokenData->logoURI) {
-//
-//  } else {
-    $image = file_get_contents('icons/binanceSmartChain/bnb.png');
-  //}
+  $json_tokenDataIcons = file_get_contents('https://token-data.unifi.report/api/getInfo?token=' . $selectedToken . '&chain=BSC');
+  $tokenDataIcons = json_decode($json_tokenDataIcons);
+  $tokenDataIconData = $tokenDataIcons->logoURI;
+  $image = file_get_contents($tokenDataIconData);
 }
-
 
 echo $image;
