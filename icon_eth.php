cc0e067f25661f707e3c9ea803434fd12e0236ee
Bart <bart@meierij-it.nl>
2021-06-15 10:24:47 +0200

    Update icon_eth.php

--- icon_eth.php
+++ icon_eth.php
@@ -5,6 +5,12 @@ $filename = 'icons/ethereum/' . $selectedToken . '.png';
 if (file_exists($filename)) {
   $image = file_get_contents('icons/ethereum/' . $selectedToken . '.png');
 } else {
-  $image = file_get_contents('icons/ethereum/eth.png');
+  $json_tokenDataIcons = file_get_contents('https://token-data.unifi.report/api/getInfo?token=' . $selectedToken . '&chain=ETH');
+  $tokenDataIcons = json_decode($json_tokenDataIcons);
+  $tokenDataIconData = $tokenDataIcons->logoURI;
+  $image = file_get_contents($tokenDataIconData);
+  if ($image){}else{
+    $image = file_get_contents('icons/ethereum/eth.png');
+  }
 }
 echo $image;
