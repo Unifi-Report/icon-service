a4ac86edce52b13baa5a39eddaeb100ad21b986b
Bart <bart@meierij-it.nl>
2021-06-17 15:16:49 +0200

    Update icon_eth.php

--- icon_eth.php
+++ icon_eth.php
@@ -1,5 +1,6 @@
 <?php
 $selectedToken = $_GET['token'];
+$autoResolve = $_GET['autoResolve'];
 $filename = 'icons/ethereum/' . $selectedToken . '.png';
 
 if (file_exists($filename)) {
@@ -9,8 +10,15 @@ if (file_exists($filename)) {
   $tokenDataIcons = json_decode($json_tokenDataIcons);
   $tokenDataIconData = $tokenDataIcons->logoURI;
   $image = file_get_contents($tokenDataIconData);
-  if ($image){}else{
-    $image = file_get_contents('icons/ethereum/eth.png');
+  if ($image) {
+  else {
+      if ($autoResolve === 'false') {
+        http_response_code(404);
+        die();
+      } else {
+        $image = file_get_contents('icons/ethereum/eth.png');
+      }
+    }
   }
 }
-echo $image;
+  echo $image;
