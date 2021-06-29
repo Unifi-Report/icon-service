0143e4f3bffe09ead9e75271c1f9b1ddfa34236e
Bart <bart@meierij-it.nl>
2021-06-29 15:18:23 +0200

    Update icon_bsc.php

--- icon_bsc.php
+++ icon_bsc.php
@@ -9,13 +9,14 @@ if (file_exists($filename)) {
   $image = file_get_contents('icons/binanceSmartChain/' . strtolower($selectedToken) . '.png');
   if ($image) {
   } else {
+    $image = file_get_contents('https://github.com/trustwallet/assets/raw/master/blockchains/smartchain/assets/' . $selectedToken . '/logo.png');
+    if ($image) {
+    } else {
     $json_tokenDataIcons = file_get_contents('https://token-data.unifi.report/api/getInfo?token=' . $selectedToken . '&chain=BSC');
     $tokenDataIcons = json_decode($json_tokenDataIcons);
     $tokenDataIconData = $tokenDataIcons->logoURI;
     $image = file_get_contents($tokenDataIconData);
-    if ($image) {
-    } else {
-      $image = file_get_contents('https://assets.trustwalletapp.com/blockchains/smartchain/assets/' . $selectedToken . '/logo.png');
+
     }
     if ($image) {
       $file = 'icons/binanceSmartChain/' . $selectedToken . '.png';
