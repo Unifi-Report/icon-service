447eb4adee2fd8b0af2c16ab9b8bc6a33809ca7b
Bart <bart@meierij-it.nl>
2021-08-04 14:11:32 +0200

    Cleanup

--- icon_iotx.php
+++ icon_iotx.php
@@ -1,15 +1,4 @@
 <?php
-//$selectedToken = $_GET['token'];
-//$filename = 'icons/iotex/' . $selectedToken . '.png';
-//
-//if (file_exists($filename)) {
-//  $image = file_get_contents('icons/iotex/' . $selectedToken . '.png');
-//} else {
-//  $image = file_get_contents('icons/unknown.png');
-//}
-//echo $image;
-
-
 $selectedToken = $_GET['token'];
 $autoResolve = $_GET['autoResolve'];
 $filename = 'icons/iotex/' . $selectedToken . '.png';
