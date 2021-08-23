65575c37032e5cce7b5b5ef7b18af8696d6e5513
Bart <bart@meierij-it.nl>
2021-08-23 10:30:47 +0200

    Adding redirect for improving speed

--- icon_trc20.php
+++ icon_trc20.php
@@ -4,7 +4,9 @@ $selectedToken = $_GET['token'];
 $filename = 'icons/tron/trc20/' . $selectedToken . '.png';
 
 if (file_exists($filename)) {
-  $image = file_get_contents('icons/tron/trc20/' . $selectedToken . '.png');
+  //$image = file_get_contents('icons/tron/trc20/' . $selectedToken . '.png');
+  header('Location: icons/tron/trc20/' . $selectedToken . '.png');
+  exit;
 } else {
   $json_TronscanIcons = file_get_contents('https://apilist.tronscan.org/api/token_trc20?contract=' . $selectedToken . '');
   $TronscanIcons = json_decode($json_TronscanIcons);
@@ -15,7 +17,7 @@ if (file_exists($filename)) {
     $image = file_get_contents($tronscanIconData[0]->icon_url);
   }
   if ($tronscanIconData[0]->icon_url === null) {
-    $image = file_get_contents('icons/unknown.png');
+    header('Location: icons/unknown.png');
   }
 }
 
