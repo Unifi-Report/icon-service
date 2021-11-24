99ce235db1130b65844e73181a06e356bcf9fd1b
Bart <bart@meierij-it.nl>
2021-11-24 16:41:12 +0100

    Update icon_bttc.php

--- icon_bttc.php
+++ icon_bttc.php
@@ -4,11 +4,11 @@ $selectedToken = $_GET['token'];
 $filename = 'icons/tron/trc20/' . $selectedToken . '.png';
 // https://scanapi.bittorrentchain.io/api/token_trc20?contract=0x76c6a7a3d535d015b9fef4f641a8fa1668424542&showAll=1
 if (file_exists($filename)) {
-  $image = file_get_contents('icons/tron/trc20/' . $selectedToken . '.png');
+  $image = file_get_contents('icons/bitTorrentChain/' . $selectedToken . '.png');
 } else {
-  $json_TronscanIcons = file_get_contents('https://apilist.tronscan.org/api/token_trc20?contract=' . $selectedToken . '');
-  $TronscanIcons = json_decode($json_TronscanIcons);
-  $tronscanIconData = $TronscanIcons->trc20_tokens;
+  $json_BTTscanIcons = file_get_contents('https://scanapi.bittorrentchain.io/api/token_trc20??contract=' . $selectedToken . '');
+  $BTTscanIcons = json_decode($json_BTTscanIcons);
+  $tronscanIconData = $BTTscanIcons->trc20_tokens;
   if ($tronscanIconData[0]->icon_url == 'https://coin.top/production/upload/logo/default.png') {
     $image = file_get_contents('https://raw.githubusercontent.com/BitGuildPlatform/dapps/master/tron/trc20/' . $selectedToken . '.png');
   } else {
