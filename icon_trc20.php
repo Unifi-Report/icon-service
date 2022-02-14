5a9937e9c99f83c99410f8da1cd1764f41f23ac1
Bart <bart@meierij-it.nl>
2022-02-14 13:47:02 +0100

    lower case support

--- icon_trc20.php
+++ icon_trc20.php
@@ -1,5 +1,5 @@
 <?php
-header ('Content-Type: image/png');
+header('Content-Type: image/png');
 
 $autoResolve = $_GET['autoResolve'];
 $selectedToken = $_GET['token'];
@@ -8,22 +8,26 @@ $filename = 'icons/tron/trc20/' . $selectedToken . '.png';
 if (file_exists($filename)) {
   $image = file_get_contents('icons/tron/trc20/' . $selectedToken . '.png');
 } else {
-  $json_BTTscanIcons = file_get_contents('https://apilist.tronscan.org/api/token_trc20?contract=' . $selectedToken . '');
-  $BTTscanIcons = json_decode($json_BTTscanIcons);
-  $bttcScanIconData = $BTTscanIcons->trc20_tokens;
-
-  if ($bttcScanIconData[0]->icon_url === null) {
-    if ($autoResolve === 'false') {
-      http_response_code(404);
-      die();
-    } else {
-      $image = file_get_contents('icons/unknown.png');
-    }
+  $image = file_get_contents('icons/tron/trc20/' . strtolower($selectedToken) . '.png');
+  if ($image) {
   } else {
-    $image = file_get_contents($bttcScanIconData[0]->icon_url);
-    file_put_contents($file, $image, FILE_APPEND | LOCK_EX);
-    $file = 'icons/tron/trc20/' . strtolower($selectedToken) . '.png';
-    file_put_contents($file, $image, FILE_APPEND | LOCK_EX);
+    $json_BTTscanIcons = file_get_contents('https://apilist.tronscan.org/api/token_trc20?contract=' . $selectedToken . '');
+    $TRXscanIcons = json_decode($json_BTTscanIcons);
+    $tronScanIconData = $TRXscanIcons->trc20_tokens;
+
+    if ($tronScanIconData[0]->icon_url === null) {
+      if ($autoResolve === 'false') {
+        http_response_code(404);
+        die();
+      } else {
+        $image = file_get_contents('icons/unknown.png');
+      }
+    } else {
+      $image = file_get_contents($tronScanIconData[0]->icon_url);
+      file_put_contents($file, $image, FILE_APPEND | LOCK_EX);
+      $file = 'icons/tron/trc20/' . strtolower($selectedToken) . '.png';
+      file_put_contents($file, $image, FILE_APPEND | LOCK_EX);
+    }
   }
 }
 
