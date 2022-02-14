9d521cbd969e3d6b8e2cf046d3a0eb1ead3a8021
Bart <bart@meierij-it.nl>
2022-02-14 12:48:14 +0100

    Add autoResolve

--- icon_trc10.php
+++ icon_trc10.php
@@ -4,13 +4,20 @@ header ('Content-Type: image/png');
 //ini_set('display_errors', '1');
 
 $selectedToken = $_GET['token'];
+$autoResolve = $_GET['autoResolve'];
+
 $filename = 'icons/tron/trc10/' . $selectedToken . '.png';
 
 //
 if (file_exists($filename)) {
 	$image = file_get_contents('icons/tron/trc10/' . $selectedToken . '.png');
 } else {
-
+  if ($autoResolve === 'false') {
+    http_response_code(404);
+    die();
+  } else {
+    $image = file_get_contents('icons/unknown.png');
+  }
 }
 
 echo $image;
