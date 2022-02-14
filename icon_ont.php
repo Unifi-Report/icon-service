b1a204850cc5a5f6ccb6db6a5fbf8fcc13c6b742
Bart <bart@meierij-it.nl>
2022-02-14 15:51:10 +0100

    Update icon_ont.php

--- icon_ont.php
+++ icon_ont.php
@@ -9,30 +9,34 @@ $filename = 'icons/ontology/' . $selectedToken . '.png';
 
 if (file_exists($filename)) {
   $image = file_get_contents('icons/ontology/' . $selectedToken . '.png');
-} else {
-  $jsonExplorerIcons_oep4 = file_get_contents('https://explorer.ont.io/v2/tokens/oep4/' . $selectedToken . '');
-  $ExplorerIcons_oep4 = json_decode($jsonExplorerIcons_oep4);
-
-  if ($ExplorerIcons_oep4->msg == 'SUCCESS') {
-    $image = file_get_contents($ExplorerIcons_oep4->result->logo);
+}else {
+  $image = file_get_contents('icons/ontology/' . strtolower($selectedToken) . '.png');
+  if ($image) {
   } else {
-    $jsonExplorerIcons_oep5 = file_get_contents('https://explorer.ont.io/v2/tokens/oep5/' . $selectedToken . '');
-    $ExplorerIcons_oep5 = json_decode($jsonExplorerIcons_oep5);
+    $jsonExplorerIcons_oep4 = file_get_contents('https://explorer.ont.io/v2/tokens/oep4/' . $selectedToken . '');
+    $ExplorerIcons_oep4 = json_decode($jsonExplorerIcons_oep4);
 
-    if ($ExplorerIcons_oep5->msg == 'SUCCESS') {
-      $image = file_get_contents($ExplorerIcons_oep5->result->logo);
+    if ($ExplorerIcons_oep4->msg == 'SUCCESS') {
+      $image = file_get_contents($ExplorerIcons_oep4->result->logo);
     } else {
-      $jsonExplorerIcons_oep8 = file_get_contents('https://explorer.ont.io/v2/tokens/oep8/' . $selectedToken . '');
-      $ExplorerIcons_oep8 = json_decode($jsonExplorerIcons_oep8);
+      $jsonExplorerIcons_oep5 = file_get_contents('https://explorer.ont.io/v2/tokens/oep5/' . $selectedToken . '');
+      $ExplorerIcons_oep5 = json_decode($jsonExplorerIcons_oep5);
 
-      if ($ExplorerIcons_oep8->msg == 'SUCCESS') {
-        $image = file_get_contents($ExplorerIcons_oep8->result->logo);
+      if ($ExplorerIcons_oep5->msg == 'SUCCESS') {
+        $image = file_get_contents($ExplorerIcons_oep5->result->logo);
       } else {
-        if ($autoResolve === 'false') {
-          http_response_code(404);
-          die();
+        $jsonExplorerIcons_oep8 = file_get_contents('https://explorer.ont.io/v2/tokens/oep8/' . $selectedToken . '');
+        $ExplorerIcons_oep8 = json_decode($jsonExplorerIcons_oep8);
+
+        if ($ExplorerIcons_oep8->msg == 'SUCCESS') {
+          $image = file_get_contents($ExplorerIcons_oep8->result->logo);
         } else {
-          $image = file_get_contents('icons/unknown.png');
+          if ($autoResolve === 'false') {
+            http_response_code(404);
+            die();
+          } else {
+            $image = file_get_contents('icons/unknown.png');
+          }
         }
       }
     }
