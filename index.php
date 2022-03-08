253050367750e1dedfc16a647e7f0a1eed25acea
Bart <bart@meierij-it.nl>
2022-03-08 13:00:07 +0100

    addin hbar

--- index.php
+++ index.php
@@ -131,6 +131,17 @@
           </div>
         </div>
       </form>
+      <h3>HBAR</h3>
+      <form class="needs-validation" novalidate action="icon_hbar" method="get">
+        <div class="row">
+          <div class="input-group mb-3 pt-1 col-10">
+            <input type="text" class="form-control" name="token" placeholder="Token Address">
+          </div>
+          <div class="col-2">
+            <button class="btn btn-danger mt-1 w-100" type="submit">Let's go</button>
+          </div>
+        </div>
+      </form>
     </div>
   </div>
 </div>
