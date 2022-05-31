7ae60f2d374a7c8f59059d7f6e054427509fee22
Bart <2702879+BaCoNL@users.noreply.github.com>
2022-05-31 11:00:09 +0200

    Create icon_sys.php

---
+++ icon_sys.php
@@ -0,0 +1,5 @@
+<?php
+
+
+// https://raw.githubusercontent.com/Pollum-io/pegasys-tokenlists/master/pegasys.tokenlist.json
+// https://raw.githubusercontent.com/Pollum-io/pegasys-tokenlists/master/tanembaum.tokenlist.json
